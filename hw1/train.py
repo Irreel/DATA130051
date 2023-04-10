2d18cd0d30e8293691fa793e1a6bdb37971ed0a4
Irreel <phantasy0@qq.com>
2023-04-10 17:02:43 +0800

    Update readme

--- hw1/train.py
+++ hw1/train.py
@@ -15,7 +15,7 @@ DECAY_PER_EPOCH = 150
 # Grid Search
 L_RATE = 1e-3
 DIM_HID = 196
-LAMBDA = 0.5 # regularization term
+LAMBDA = 0.25 # regularization term
 
 random.seed(52)
 
@@ -55,7 +55,7 @@ def training(epoch, learning_rate, reg_term, model):
         true_label = np.argmax(y_label, axis = 1)
         valid_acc_ls.append(np.sum(pred_label == true_label) / len(true_label))
     
-    return np.mean(train_loss_ls), np.mean(valid_loss_ls), np.mean(valid_acc_ls)
+    return (train_loss_ls), (valid_loss_ls), np.mean(valid_loss_ls), np.mean(valid_acc_ls)
 
 
 def main(model, epoch=EPOCH, lr_start=L_RATE, reg_term=LAMBDA):
@@ -72,14 +72,14 @@ def main(model, epoch=EPOCH, lr_start=L_RATE, reg_term=LAMBDA):
         # Decay strategy
         if i % DECAY_PER_EPOCH == 0: 
             lr *= DECAY_FACTOR
-        train_loss, valid_loss, valid_acc = training(i, lr, reg_term, model)
+        train_loss, valid_loss, valid_loss_avg, valid_acc = training(i, lr, reg_term, model)
         acc_ls.append(valid_acc)
         val_loss_ls.append(valid_loss)
         train_loss_ls.append(train_loss)
-        if valid_loss <= val_loss_best and valid_acc >= acc_best:
+        if valid_loss_avg <= val_loss_best and valid_acc >= acc_best:
             epoch_best = i
             acc_best = valid_acc
-            val_loss_best = valid_loss
+            val_loss_best = valid_loss_avg
             model_best = model
     
     # Save model and parameters
@@ -94,6 +94,6 @@ def main(model, epoch=EPOCH, lr_start=L_RATE, reg_term=LAMBDA):
 
 if __name__ == "__main__":
     _, acc, acc_ls, train_loss_ls, val_loss_ls = main(class_model)
-    vis_loss(train_loss_ls, val_loss_ls, EPOCH, save_flg=True)
+    vis_loss(np.array(train_loss_ls).ravel(), val_loss_ls, save_flg=True)
     vis_acc(acc_ls, EPOCH, save_flg=True)
     
